f08ed9e67a041e48e168a176ddf0314d197f85c4
Stephen <skent89@gmail.com>
2023-06-29 11:53:40 -0400

    updated postgres_config.py

--- postgres_config.py
+++ postgres_config.py
@@ -1,5 +1,5 @@
-POSTGRES_USER = 'skent'
-POSTGRES_PASSWORD = 'password'
+POSTGRES_USER = 'your user name'
+POSTGRES_PASSWORD = 'your password'
 POSTGRES_HOST = 'localhost' # your host here probably 
 POSTGRES_PORT = '5432' #your port here, probably 5432
 POSTGRES_DB = 'db_nypd_calls_for_service_data' 
